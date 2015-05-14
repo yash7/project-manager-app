b0c0fc09676b2f10c88827db64f2fb8feab7ce1d
Anthony-Virgil Bermejo <anthonyvirgil.bermejo@gmail.com>
2015-05-14 16:50:38 -0400

    Added JUnit libraries and sample JUnit test

---
+++ src/ateamcomp354/projectmanagerapp/testing/SampleTest.java
@@ -0,0 +1,18 @@
+package ateamcomp354.projectmanagerapp.testing;
+
+import static org.junit.Assert.*;
+import ateamcomp354.projectmanagerapp.model.Ateam;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+
+		// 2 * 2 == 4
+		assertEquals(Ateam.f(2), 4);
+		
+	}
+
+}
