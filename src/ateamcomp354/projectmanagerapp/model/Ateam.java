3f49ac0defe3c08d369f7431f69d881b0bca8cf7
kyle-ilantzis <ilantzis.kyle@gmail.com>
2015-05-20 05:15:38 -0400

    deprecate dumb Ateam class

--- src/ateamcomp354/projectmanagerapp/model/Ateam.java
+++ src/ateamcomp354/projectmanagerapp/model/Ateam.java
@@ -1,5 +1,8 @@
 package ateamcomp354.projectmanagerapp.model;
 
+/**
+ * @deprecated - this was just for setup and demo purposes, this needs to be deleted.
+ */
 public class Ateam {
 
 	public static int f( int x ) {
