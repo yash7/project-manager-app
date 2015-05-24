81ba6987abd8321eae50dac747ea54aee7fdf996
Anthony-Virgil Bermejo <anthonyvirgil.bermejo@gmail.com>
2015-05-24 18:58:10 -0400

    Project Service Test complete

--- src/ateamcomp354/projectmanagerapp/testing/ProjectServiceTest.java
+++ src/ateamcomp354/projectmanagerapp/testing/ProjectServiceTest.java
@@ -6,27 +6,97 @@ import ateamcomp354.projectmanagerapp.model.Status;
 import ateamcomp354.projectmanagerapp.services.ActivityService;
 import ateamcomp354.projectmanagerapp.services.ApplicationContext;
 import ateamcomp354.projectmanagerapp.services.ProjectService;
+
 import org.jooq.ateamcomp354.projectmanagerapp.tables.pojos.Activity;
 import org.jooq.ateamcomp354.projectmanagerapp.tables.pojos.Project;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ProjectServiceTest {
 
-    /**
-     * Just to make sure the JOOQ select statements work as expected.
-     */
+	@Test
+	public void testGetProjects_noProjects() throws Exception {
+		
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        
+        List<Project> projectList = projectService.getProjects();
+        assertEquals(0, projectList.size());
+	}
+	
+	@Test
+	public void testGetProjects_addedProject() throws Exception {
+		
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        
+        Project p = new Project();
+        p.setId( 0 );
+        p.setProjectName("Test project");
+        p.setDescription("Test project");
+
+        projectService.addProject( p );
+        
+        List<Project> projectList = projectService.getProjects();
+        assertEquals(1, projectList.size());
+	}
+	
+	@Test
+	public void testGetProject_noProjects() throws Exception {
+		
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        
+        Project p = projectService.getProject(0);
+        
+        assertNull(p);
+	}
+	
+	@Test
+	public void testGetProject_projectExists() throws Exception {
+		
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        
+        Project p = new Project();
+        p.setId(0);
+        p.setProjectName("Test project");
+        p.setDescription("Test project");
+        
+        projectService.addProject( p );
+
+        Project test = projectService.getProject(0);
+        
+        assertNotNull(test);
+        assertEquals("Test project", test.getProjectName());
+        assertEquals("Test project", test.getDescription());
+        assertEquals((int)test.getId(), 0);
+	}
+	
     @Test
-    public void testGetProjectCompletion_DoesItWork() throws Exception {
+    public void testGetProjectCompletion_halfComplete() throws Exception {
 
         final int projectId = 0;
 
         File file = Files.createTempFile( "test", "db" ).toFile();
-        file.deleteOnExit();
 
         DatabaseManager db = new DatabaseManager( file.getName() );
         ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
@@ -56,5 +126,220 @@ public class ProjectServiceTest {
                 50,
                 projectService.getProjectCompletion( projectId )
         );
+        
+        file.delete();
     }
+    
+    @Test
+    public void testGetProjectCompletion_zeroComplete() throws Exception {
+
+        final int projectId = 0;
+
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        ActivityService activityService = appCtx.getActivityService( projectId );
+
+        Project p = new Project();
+        p.setId( projectId );
+        p.setProjectName("Test project");
+        p.setDescription("Test project");
+
+        projectService.addProject( p );
+
+        Activity a1 = new Activity();
+        a1.setProjectId( projectId );
+        a1.setLabel( "a1" );
+
+        Activity a2 = new Activity();
+        a2.setProjectId( projectId );
+        a2.setLabel( "a2" );
+
+        activityService.addActivity( a1 );
+        activityService.addActivity( a2 );
+
+        assertEquals(
+                0,
+                projectService.getProjectCompletion( projectId )
+        );
+        
+        file.delete();
+    }
+    
+    @Test
+    public void testGetProjectCompletion_noActivityZeroComplete() throws Exception {
+
+        final int projectId = 0;
+
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        ActivityService activityService = appCtx.getActivityService( projectId );
+
+        Project p = new Project();
+        p.setId( projectId );
+        p.setProjectName("Test project");
+        p.setDescription("Test project");
+
+        projectService.addProject( p );
+
+        assertEquals(
+                0,
+                projectService.getProjectCompletion( projectId )
+        );
+        
+        file.delete();
+    }
+    
+    @Test
+    public void testGetProjectCompletion_complete() throws Exception {
+
+        final int projectId = 0;
+
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        ActivityService activityService = appCtx.getActivityService( projectId );
+
+        Project p = new Project();
+        p.setId( projectId );
+        p.setProjectName("Test project");
+        p.setDescription("Test project");
+
+        projectService.addProject( p );
+
+        Activity a1 = new Activity();
+        a1.setProjectId( projectId );
+        a1.setLabel( "a1" );
+        a1.setStatus(Status.RESOLVED.ordinal());
+
+        Activity a2 = new Activity();
+        a2.setProjectId( projectId );
+        a2.setLabel( "a2" );
+        a2.setStatus(Status.RESOLVED.ordinal());
+
+        activityService.addActivity( a1 );
+        activityService.addActivity( a2 );
+
+        assertEquals(
+                100,
+                projectService.getProjectCompletion( projectId )
+        );
+        
+        file.delete();
+    }
+    
+	@Test
+	public void testAddProject_nullProject() throws Exception {
+		
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        
+        Project test = null;
+        
+        projectService.addProject( test );
+        
+        
+        assertEquals(0, projectService.getProjects().size());
+
+        assertNull(test);
+	}
+	
+	@Test
+	public void testAddProject_newProject() throws Exception {
+		
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        
+        Project p = new Project();
+        p.setId(0);
+        p.setProjectName("Test project");
+        p.setDescription("Test project");
+
+        
+        projectService.addProject(p);
+        
+        assertEquals(1, projectService.getProjects().size());
+	}
+	
+	@Test
+	public void testDeleteProject_noProductExistsWithID() throws Exception {
+		
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        
+        projectService.deleteProject(0);
+        
+	}
+	
+	@Test
+	public void testDeleteProject_productExists() throws Exception {
+		
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        
+        Project p = new Project();
+        p.setId(0);
+        p.setProjectName("Test project");
+        p.setDescription("Test project");
+
+        projectService.addProject(p);
+        
+        assertEquals(1, projectService.getProjects().size());
+        
+        projectService.deleteProject(0);
+        
+        assertEquals(0, projectService.getProjects().size());
+        
+	}
+	
+	
+	
+	@Test
+	public void testUpdateProject_productExists() throws Exception {
+		
+        File file = Files.createTempFile( "test", "db" ).toFile();
+
+        DatabaseManager db = new DatabaseManager( file.getName() );
+        ApplicationContext appCtx = App.getApplicationContext( db.getOpenConnection() );
+        ProjectService projectService = appCtx.getProjectService();
+        
+        Project p = new Project();
+        p.setId(0);
+        p.setProjectName("Test project");
+        p.setDescription("Test project");
+
+        projectService.addProject(p);
+        
+        Project test = projectService.getProject(0);
+        
+        test.setDescription("New description");
+        test.setProjectName("New project name");
+        
+        projectService.updateProject(test);
+        
+        Project finalTest = projectService.getProject(0);
+        
+        assertEquals(0, (int)finalTest.getId());
+        assertEquals("New description", finalTest.getDescription());
+        assertEquals("New project name", finalTest.getProjectName());
+	}
 }
