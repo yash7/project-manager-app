60f2e2624962354e3ddda3d30f02d071f62754c1
kyle-ilantzis <ilantzis.kyle@gmail.com>
2015-05-27 21:30:42 -0400

    ActivityService implementation for dependency functions ( graph edges )

--- src/ateamcomp354/projectmanagerapp/dataAccess/ddl/UserActivities.sql
+++ src/ateamcomp354/projectmanagerapp/dataAccess/ddl/UserActivities.sql
@@ -1,4 +1,6 @@
 CREATE TABLE userActivities (
+	id INTEGER PRIMARY KEY, -- a record if for jooq code generation
 	activity_id INTEGER REFERENCES activity (id),
-	user_id INTEGER REFERENCES users (id)
+	user_id INTEGER REFERENCES users (id),
+	UNIQUE ( activity_id, user_id )
 );
