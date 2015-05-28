e78f471831a20323c948545aad9d0eee8ff1aa3a
kyle-ilantzis <ilantzis.kyle@gmail.com>
2015-05-28 14:58:02 -0400

    cascade deletes on acitivites and users

--- src/ateamcomp354/projectmanagerapp/dataAccess/ddl/UserActivities.sql
+++ src/ateamcomp354/projectmanagerapp/dataAccess/ddl/UserActivities.sql
@@ -1,6 +1,6 @@
 CREATE TABLE userActivities (
 	id INTEGER PRIMARY KEY, -- a record if for jooq code generation
-	activity_id INTEGER REFERENCES activity (id),
-	user_id INTEGER REFERENCES users (id),
+	activity_id INTEGER REFERENCES activity (id) ON DELETE CASCADE,
+	user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
 	UNIQUE ( activity_id, user_id )
 );
