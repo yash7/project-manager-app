20f5ed2e9af7f57c09b3336676a046a82d929597
kyle-ilantzis <ilantzis.kyle@gmail.com>
2015-06-06 13:34:19 -0400

    login view is centered

--- src/ateamcomp354/projectmanagerapp/ui/gen/LoginPanelGen.java
+++ src/ateamcomp354/projectmanagerapp/ui/gen/LoginPanelGen.java
@@ -7,46 +7,96 @@ import javax.swing.JButton;
 import javax.swing.JPasswordField;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class LoginPanelGen extends JPanel {
 	private JTextField usernameField;
 	private JButton oKButton;
-	private JButton cancelButton;
 	private JPasswordField passwordField;
 	private JLabel validityLabel;
+	private JPanel panel;
+	private Component verticalGlue;
+	private Component verticalGlue_1;
 
 	/**
 	 * Create the panel.
 	 */
 	public LoginPanelGen() {
-		setLayout(null);
+		setLayout(new BorderLayout(0, 0));
+		
+		verticalGlue = Box.createVerticalGlue();
+		add(verticalGlue, BorderLayout.NORTH);
+		
+		verticalGlue_1 = Box.createVerticalGlue();
+		add(verticalGlue_1, BorderLayout.SOUTH);
+		
+		panel = new JPanel();
+		add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{300};
+		gbl_panel.rowHeights = new int[]{23, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		panel.setLayout(gbl_panel);
 		
 		JLabel usernameLabel = new JLabel("Username:");
-		usernameLabel.setBounds(140, 70, 70, 16);
-		add(usernameLabel);
+		GridBagConstraints gbc_usernameLabel = new GridBagConstraints();
+		gbc_usernameLabel.anchor = GridBagConstraints.WEST;
+		gbc_usernameLabel.insets = new Insets(0, 0, 10, 0);
+		gbc_usernameLabel.gridx = 0;
+		gbc_usernameLabel.gridy = 1;
+		panel.add(usernameLabel, gbc_usernameLabel);
 		
 		JLabel passwordLabel = new JLabel("Password:");
-		passwordLabel.setBounds(140, 135, 70, 16);
-		add(passwordLabel);
+		GridBagConstraints gbc_passwordLabel = new GridBagConstraints();
+		gbc_passwordLabel.insets = new Insets(0, 0, 10, 0);
+		gbc_passwordLabel.anchor = GridBagConstraints.WEST;
+		gbc_passwordLabel.gridx = 0;
+		gbc_passwordLabel.gridy = 3;
+		panel.add(passwordLabel, gbc_passwordLabel);
 		
 		usernameField = new JTextField();
-		usernameField.setBounds(140, 100, 116, 22);
-		add(usernameField);
-		usernameField.setColumns(10);
+		GridBagConstraints gbc_usernameField = new GridBagConstraints();
+		gbc_usernameField.ipady = 5;
+		gbc_usernameField.ipadx = 5;
+		gbc_usernameField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_usernameField.anchor = GridBagConstraints.WEST;
+		gbc_usernameField.insets = new Insets(0, 0, 20, 0);
+		gbc_usernameField.gridx = 0;
+		gbc_usernameField.gridy = 2;
+		panel.add(usernameField, gbc_usernameField);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(140, 165, 116, 22);
-		add(passwordField);
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.ipady = 5;
+		gbc_passwordField.ipadx = 5;
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.anchor = GridBagConstraints.WEST;
+		gbc_passwordField.insets = new Insets(0, 0, 20, 0);
+		gbc_passwordField.gridx = 0;
+		gbc_passwordField.gridy = 4;
+		panel.add(passwordField, gbc_passwordField);
 		
 		oKButton = new JButton("OK");
-		oKButton.setBounds(140, 206, 90, 25);
-		add(oKButton);
+		GridBagConstraints gbc_oKButton = new GridBagConstraints();
+		gbc_oKButton.insets = new Insets(0, 0, 0, 5);
+		gbc_oKButton.gridx = 0;
+		gbc_oKButton.gridy = 5;
+		panel.add(oKButton, gbc_oKButton);
 		
 		validityLabel = new JLabel("");
+		GridBagConstraints gbc_validityLabel = new GridBagConstraints();
+		gbc_validityLabel.insets = new Insets(0, 0, 20, 0);
+		gbc_validityLabel.gridx = 0;
+		gbc_validityLabel.gridy = 0;
+		panel.add(validityLabel, gbc_validityLabel);
 		validityLabel.setForeground(Color.RED);
 		validityLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
-		validityLabel.setBounds(140, 244, 298, 16);
-		add(validityLabel);
 
 	}
 	public JTextField getUsernameField() {
