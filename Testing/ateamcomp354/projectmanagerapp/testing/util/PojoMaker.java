67a9eca683b6c719522056148db364bdaaf4ad6d
kyle-ilantzis <ilantzis.kyle@gmail.com>
2015-07-28 22:52:01 -0400

    first tests for critical paths chart
    refactored the date into and int logic
    so that it could be reused when making activity objects
    in tests.
    
    2 critical paths chart test fail

---
+++ Testing/ateamcomp354/projectmanagerapp/testing/util/PojoMaker.java
@@ -0,0 +1,45 @@
+package ateamcomp354.projectmanagerapp.testing.util;
+
+import ateamcomp354.projectmanagerapp.model.Pojos;
+import ateamcomp354.projectmanagerapp.model.Status;
+import org.jooq.ateamcomp354.projectmanagerapp.tables.pojos.Activity;
+
+import java.util.Calendar;
+
+/**
+ * Functions to help make objects more easily,
+ *
+ * suggested to static import * this class for even more ease.
+ */
+public class PojoMaker {
+
+    public static int activityIdSeq = 0;
+
+    /**
+     * Util to make a Calendar object
+     *
+     * @param year YYYY
+     * @param month 1-based, 1 = January and 12 = December
+     * @param day 1-based day of the month
+     * @return
+     */
+    public static Calendar makeDate(int year, int month, int day) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(0);
+        c.set(year, month - 1, day);
+        return c;
+    }
+
+    public static Activity makeActivity(int projectId, String name, Calendar start, Calendar end) {
+        Activity a = new Activity();
+        a.setId( activityIdSeq++ );
+        a.setProjectId( projectId );
+        a.setLabel( name );
+        a.setEarliestStart(Pojos.dateToInt( start ) );
+        a.setEarliestFinish(Pojos.dateToInt( end ) );
+        a.setDescription("");
+        a.setStatus(Status.NEW);
+        a.setPlannedValue(0);
+        return a;
+    }
+}
